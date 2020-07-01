db66bad31261efbc3b7caaa37ffa91c5d75fb4f9
Teodoro Garcia <teodorogarcia@teodoros-air.lan>
2020-07-01 18:43:42 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Dog
+    # def initialize(name, breed)
+    #     @name= name
+    #     @breed= breed
+    # end
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+    # binding.pry
+end
+
+# fido = Dog.new
+# fido.name = "Fido"
+# fido.breed = "Beagle"
